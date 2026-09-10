db512cf6a73617f62242173a2d7184dc54593135
dalmeidasdev <dalmeidasdev@gmail.com>
2026-09-10 20:48:54 -0300

    Desafio 031

---
+++ mundo-1/exercicios/desafio031.py
@@ -0,0 +1,11 @@
+# Custo da viagem
+# Link do vídeo: 
+
+d = float(input('Digite a distância da viagem em km: '))
+
+if d <= 200:
+    s = d * 0.5
+    print(f'O custo da viagem é de R${s:.2f}')
+else:
+    s = d * 0.45
+    print(f'O custo da viagem é de R${s:.2f}')
