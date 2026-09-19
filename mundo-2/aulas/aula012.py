96e84f812254d07bc9b5deb26903e411c062a486
dalmeidasdev <dalmeidasdev@gmail.com>
2026-09-19 08:31:21 -0300

    mundo 2

---
+++ mundo-2/aulas/aula012.py
@@ -0,0 +1,11 @@
+# Condições aninhadas
+# Link do vídeo: https://youtu.be/j9bYDjaAYzw?si=DovrS4paMjwiJMef
+
+nome = input('Qual o seu nome?: ').strip().upper()
+
+if nome == 'DIEGO':
+    print('Seu nome é igual o meu !')
+elif nome == 'KAIO' or nome == 'NEYSER':
+    print('Seu nome é igual o do atacante do Cruzeiro !')
+else:
+    print('Ok')
---
+++ mundo-2/exercícios/desafio036.py
@@ -0,0 +1,15 @@
+# Aprovando empréstimos
+# Link do vídeo:
+
+vcasa = float(input('Digite o valor da casa: '))
+salario = float(input('Digite o seu salário: '))
+anos = int(input('Em quantos anos pretende pagar?: '))
+
+meses = anos * 12
+prestacao = vcasa / meses
+limite = (30/100) * salario
+
+if prestacao > limite:
+    print('Empréstimo negado !')
+else:
+    print('Empréstimo aprovado !')
